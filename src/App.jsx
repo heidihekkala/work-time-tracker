b2466ab9663457054c9f9cba62999cd51c0f5182
Heidi Hekkala <ag7715@student.jamk.fi>
2025-01-12 23:48:31 +0200

    muokattu h2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 //import { useState } from 'react'
 import TyöAjanSeuranta from './WorkHourInput'
-import './App.css'
+import './App.css';
 
 function App() {
   return (
     <div>
-      <h1>Oma sovellukseni</h1>
+      <h2>Työajan seuranta:</h2>
       <TyöAjanSeuranta />
     </div>
   );
