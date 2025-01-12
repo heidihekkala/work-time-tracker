803be6f9223eee9cd882a3156208d318a690e7e1
Heidi Hekkala <ag7715@student.jamk.fi>
2025-01-12 23:43:16 +0200

    add WorkHourInput.jsx and delete WorkHourInput.js

---
+++ src/WorkHourInput.jsx
@@ -0,0 +1,9 @@
+//import React from 'react';
+//import { useForm } from 'react-hook-form';
+
+const TyöAjanSeuranta = () => {
+    return <div>Tästä alan rakentamaan työajanseurantalomaketta</div>;
+  };
+  
+  export default TyöAjanSeuranta;
+  
