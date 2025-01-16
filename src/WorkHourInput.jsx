2db5724c24a1d98aecbeda2d89dae391fabdb45b
Heidi Hekkala <ag7715@student.jamk.fi>
2025-01-16 13:51:07 +0200

    lisätty työnantaja-valikko

--- src/WorkHourInput.jsx
+++ src/WorkHourInput.jsx
@@ -1,9 +1,19 @@
 //import React from 'react';
 //import { useForm } from 'react-hook-form';
 
-const TyöAjanSeuranta = () => {
-    return <div>Tästä alan rakentamaan työajanseurantalomaketta</div>;
-  };
+function TyöAjanSeuranta() {
+    return (
+      <div>
+        <h2>Työajan seuranta:</h2>
+        <form>
+          <label for="työnantaja">Valitse työnantaja:</label>
+            <select id="työnantaja" name="työnantaja">
+              <option value="A">Työnantaja A</option>
+              <option value="B">Työnantaja B</option>
+            </select>
+        </form>
+      </div>
+  );}
   
   export default TyöAjanSeuranta;
   
