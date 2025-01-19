3c139a43d28da837a20ac7804ddf0ed6b39d8b45
Heidi Hekkala <ag7715@student.jamk.fi>
2025-01-19 18:49:37 +0200

    kokeiltu DateTimePicker tomintaa

--- src/WorkHourInput.jsx
+++ src/WorkHourInput.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useForm } from 'react-hook-form';
 import styled from "styled-components";
 import Button from '@mui/material/Button';
-import DateTimePicker from 'react-datetime-picker';
+//import AjanValinta from "./SetDateTime";
 
 const Container = styled.div`
   font-family: Arial, sans-serif;
